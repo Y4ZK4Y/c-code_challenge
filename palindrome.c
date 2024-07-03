8d1c6a0afd944b350e41af2c95c925833b1c7172
Y4ZK4Y <yk@yasamankarimi@gmail.com>
2024-07-03 11:46:47 +0200

    add comments to palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,4 +1,36 @@
-#include <stdio.h>
+/*
+Assignment name  : biggest_pal
+Expected files   : *.c, *.h
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program named biggest_pal that takes a string as argument and prints the
+biggest palindrome included inside this string, followed by a newline.
+
+This string contains only lowercase characters.
+
+If there is two palindromes of the same length, you have to print the last one.
+
+If there is more or less than 1 argument, your program must print only a newline
+only.
+
+Exemples:
+
+$> biggest_pal "abcba"
+abcba
+$> biggest_pal "aaaaaaaaaabbcbbaaaa"
+aaaabbcbbaaaa
+$> biggest_pal "aooibdaoiwhoihwdoinzeldaisaboyobasiadlezfdsfnslk"
+zeldaisaboyobasiadlez
+$> biggest_pal "aeibaabaammaabaalek"
+aabaammaabaa
+$> biggest_pal abeb qpinqwjobo qkmnwoiq
+
+$> biggest_pal
+
+$>
+*/
+
 #include <unistd.h>
 #include <stdbool.h>
 
@@ -6,12 +38,11 @@
 size_t ft_strlen(char *str)
 {
     size_t i = 0;
-    if (!str)
-        return 0;
     while (str[i] != '\0')
         i++;
     return (i);
 }
+
 
 bool is_palindrome(char *str, int start, int end)
 {
@@ -24,7 +55,6 @@ bool is_palindrome(char *str, int start, int end)
 }
 
 
-
 void longest_palindrome(char *str)
 {
     int start;
@@ -34,13 +64,14 @@ void longest_palindrome(char *str)
 
     len = ft_strlen(str);
     start = 0;
-    maxlen = 1;
+    maxlen = 1; //  a single character is a palindrome
 
     for (i = 0; i < len; i++)
     {
-        for (j = 0; j < len; j++)
+        for (j = i; j < len; j++)
         {
-            if (is_palindrome(str, i, j) && (j - i + 1) > maxlen) // len of the current substring
+            // checks substrings for palindromes
+            if (is_palindrome(str, i, j) && (j - i + 1) >= maxlen) // j-i+1: len of the current substring
             {
                 start = i;
                 maxlen = j - i + 1;
@@ -54,14 +85,11 @@ void longest_palindrome(char *str)
 }
 
 
-
-
 int main(int argc, char **argv)
 {
-    char *str = "Too hot to hoot";
-    longest_palindrome(str);
+    if (argc == 2)
+        longest_palindrome(argv[1]);
+    else
+        write(1, "\n", 1);
     return 0;
-
 }
-
-
